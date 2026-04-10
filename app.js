4d074ab2ad0937dd46f8daf91a5b3d8fb6f3c4dc
PogodaIgor <igor.pogoda2010@wp.pl>
2026-04-10 14:58:03 +0200

    do domu

--- app.js
+++ app.js
@@ -4,20 +4,12 @@ async function getData() {
     const data_json = await data.json()
 console.log(data_json)
 
-const list = document.querySelector(".list")
-const item = document.querySelector(".item")
+const flag = document.querySelector(".flag")
 
 for(var i=0;i<=data_json.length-1;i++){
-    const li = document.createElement("li")
-    if(data_json[i].continets[0] == "Europe"){
-        li.style.color = "blue"
-    }
-    if(data_json[i].continets.length>1){
-        li.style.color = "red"
-    }
-    
-    li.innerHTML = data_json[i].name.common
+    fg.innerHTML = data_json[i].name.common
     list.appendChild(li)
+    
 }
 }
 getData()
