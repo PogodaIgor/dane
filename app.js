235df529ad05e165b4d564b6ff7d8e2ebb2c1446
PogodaIgor <igor.pogoda2010@wp.pl>
2026-04-15 17:54:11 +0200

    a

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-
 async function getData() {
-    const data = await fetch("https://restcountries.com/v3.1/all?fields=name,capital,flags,continents")
-    const data_json = await data.json()
-console.log(data_json)
+  const data = await fetch(
+    "https://restcountries.com/v3.1/all?fields=name,flags,continents"
+  );
+  const data_json = await data.json();
 
-const flag = document.querySelector(".flag")
+  console.log(data_json);
+  const list = document.querySelector(".list");
+  const item = document.querySelector(".item");
 
-for(var i=0;i<=data_json.length-1;i++){
-    fg.innerHTML = data_json[i].name.common
-    list.appendChild(li)
-    
+  for (var i = 0; i < data_json.length; i++) {
+    const country = data_json[i];
+    const li = document.createElement("li");
+    li.innerHTML = `
+                <img class="flag" src="${country.flags.svg}" alt=".">
+                <div class="info">
+                    <h2>${country.name.common}</h2>
+                    <p>${country.continents[0]}</p>
+                </div>
+            `;
+
+    list.appendChild(li);
+  }
 }
-}
-getData()
+
+getData();
